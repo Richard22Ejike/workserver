08e03c385b82882e860077cb0f9933fdf8337f68
Richard dev <richard.ekene@aun.edu.ng>
2023-05-26 20:22:28 +0100

    second

--- index.js
+++ index.js
@@ -1,45 +1,69 @@
-const express = require ('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var helmet = require('helmet');
-
-
+const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config();
-const otpGenerator = require("otp-generator");
-const deliveryRouter = require("./routes/delivery");
+const cors = require("cors");
+const http = require("http");
+const authRouter = require("./routes/auth");
+const documentRouter = require("./routes/document");
+const Document = require("./models/document");
 
-const PORT = 3000;
-const DB = "mongodb+srv://richard:liverpool@cluster0.vxcma1z.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT | 3001;
 
 const app = express();
+var server = http.createServer(app);
+var io = require("socket.io")(server);
 
-// IMPORTS FROM OTHER FILES
-const authRouter = require("./routes/auth");
-const userRouter = require("./routes/user");
-const adminRouter = require("./routes/admin");
-// middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(express.json());
 app.use(authRouter);
-app.use(deliveryRouter);
-app.use(userRouter);
-app.use(adminRouter);
-// Connections
+app.use(documentRouter);
+
+const DB =
+  "mongodb+srv://richardekene22:liverpool@cluster0.ssvrcbo.mongodb.net/?retryWrites=true&w=majority";
+mongoose.set('strictQuery', false);
 mongoose
-  .set('strictQuery', false)
   .connect(DB)
   .then(() => {
-    console.log("Connection Successful");
+    console.log("Connection successful!");
   })
-  .catch((e) => {
-   
+  .catch((err) => {
+    console.log(err);
   });
 
-app.listen(PORT,"0.0.0.0", () => {
-     console.log(`connected at port ${PORT}`  );
-})
+io.on("connection", (socket) => {
+  socket.on("join", (documentId) => {
+    socket.join(documentId);
+  });
+
+  socket.on("typing", (data) => {
+    socket.broadcast.to(data.room).emit("changes", data);
+  });
+
+  socket.on("save", (data) => {
+    saveData(data);
+  });
+});
+
+const saveData = async (data) => {
+  let document = await Document.findById(data.room);
+  document.content = data.delta;
+
+  try {
+    document = await document.save();
+    console.log('Document saved successfully!');
+  } catch (error) {
+    if (error.name === 'VersionError') {
+      // Fetch the document again and try saving it
+      document = await Document.findById(data.room);
+      document.content = data.delta;
+      document = await document.save();
+      console.log('Document saved successfully after retrying!');
+    } else {
+      console.log('Error saving document:', error);
+    }
+  }
+};
+
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`connected at port ${PORT}`);
+});
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,10 +3,12 @@ const jwt = require("jsonwebtoken");
 const auth = async (req, res, next) => {
   try {
     const token = req.header("x-auth-token");
+
     if (!token)
-      return res.status(401).json({ msg: "No auth token, access denied" });
+      return res.status(401).json({ msg: "No auth token, access denied." });
 
     const verified = jwt.verify(token, "passwordKey");
+
     if (!verified)
       return res
         .status(401)
@@ -15,8 +17,8 @@ const auth = async (req, res, next) => {
     req.user = verified.id;
     req.token = token;
     next();
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
 };
 
